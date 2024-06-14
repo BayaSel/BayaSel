510f94d02648f93ed635a4a9fd3f211a8f976984
Victor Maduanusi <maduanusivictor2020@gmail.com>
2024-06-14 10:01:43 +0100

    Quantity price tag

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,60 @@
 import orangeImg from '../assets/cart/orange.png'
 import CartHeader from './CartHeader'
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
+import { red } from '@mui/material/colors';
+
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import './Styles/cart.css'
+import { useState } from 'react';
 
 function Footer() {
+  const [count, setCount] = useState(0)
   return (
     <div>
       <CartHeader />
-      <div className="cartbody">
-        <img src={orangeImg} alt="" />
+      <div className='px-10 py-5 my-20 bg-white-200 flex justify-between'>
+        <div>
+          <table className='text-left '>
+            <thead className='border-b-[1px] border-gray-500'>
+              <tr>
+                <th className='pb-10'>Product</th>
+                <th className='pb-10'>Price</th>
+                <th className='pb-10'>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className='py-10 pr-20'>
+                  <img src={orangeImg} alt="orange" className='inline-block mr-2' />
+                  Fresh orange
+                </td>
+                <td className='pr-10'>$250.00</td>
+                <td  className=''>
+                  <div className='flex justify-between'>
+                    <div className='flex justify-between items-center px-2 mr-2  border-2 rounded-[1rem] w-[9rem]'>
+                      <button onClick={() => {
+                        setCount(c => c === 0 ? 0 : c - 1)
+                      }}><RemoveIcon color={`${count === 0 && "disabled"}`} /></button>
+                      <p className='inline-block px-4'>{count}</p>
+                      <button onClick={() => {
+                        setCount(c => c + 1)
+                      }}><AddIcon /></button>
+                    </div>
+                    <button className='my-5'>
+                      <DeleteOutlineIcon sx={{ color: red[500] }} />
+                    </button>
+                  </div>
+                  
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div>
+          hello
+        </div>
       </div>
     </div>
   )
--- src/components/CartHeader.jsx
+++ src/components/CartHeader.jsx
@@ -1,15 +1,17 @@
 import ArrowRightRoundedIcon from '@mui/icons-material/ArrowRightRounded';
 import WestRoundedIcon from '@mui/icons-material/WestRounded';
+import { green } from '@mui/material/colors';
+
 export default function CartHeader() {
   return (
-    <div>
-        <div className="nav-cart">
-            <p>Home<ArrowRightRoundedIcon /></p>
-            <p>Shopping cart</p>
-        </div>
-        <div className="continueShopping">
-            <p>continue Shopping <WestRoundedIcon /></p>
-        </div>
+    <div  className="flex px-10 justify-between py-10 bg-gray-100">
+      <div className='flex justify-between'>
+        <p>Home<ArrowRightRoundedIcon  /></p>
+        <p className='text-gray-400'>Shopping cart</p>
+      </div>
+      <div>
+        <p className='text-green-500'>continue Shopping <WestRoundedIcon sx={{ color: green[500] }} /></p>
+      </div>
     </div>
   )
 }
