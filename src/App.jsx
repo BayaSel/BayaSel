29da6c9391291a916fdd17a8082b10c628e2a27b
Vechy <verachinwa@gmail.com>
2024-08-14 05:12:11 -0700

    Revert "Merge pull request #5 from BayaSel/dev-homepage"
    This reverts commit 133988c65c39d27c744969b4bfb4316d200ea098, reversing
    changes made to beb241d037ecf3e9489c07ad84e63b1dc8e39ad6.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import LoginForm from "./pages/LoginForm";
 import RegisterForm from "./pages/RegisterForm";
-import Home from "./components/home/Home";
 
 function App() {
   return (
-    <Router>
-      <>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<RegisterForm />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="*" element={<p>Page Not Found</p>} />
-        </Routes>
-        <Footer />
-      </>
-    </Router>
+    <>
+      {/* <Header /> */}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              {/* {" "} */}
+              <Header />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="*" element={<p>Page Not found </p>} />
+      </Routes>
+      {/* There would be a routing or Action btn in the header component(sign In) */}
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,7 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
-      margin: {
-        "-12.5": "-50px",
-      },
-      height: {
-        30: "120px",
-      },
-      colors: {
-        "4BAF47": "#4BAF47",
-        customGray: "#F3F3F3", // Add custom gray color if needed
-      },
+
     },
   },
   plugins: [],
