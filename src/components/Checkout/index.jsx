ecda92b26bd3b9a60df2be87d39078bae0065978
Siddami <dharmmy621@gmail.com>
2024-08-21 12:48:54 +0100

    working on radio element

--- src/components/Checkout/index.jsx
+++ src/components/Checkout/index.jsx
@@ -6,7 +6,7 @@ import CustomCheckbox from '../CustomCheckbox';
 const Checkout = () => {
     return (
      <>
-         <Header />
+         <Header className="fixed z-50" />
          <div className='checkout-container'>
             <section className='checkout-form'>
                 <section className='contact-info'>
@@ -85,6 +85,16 @@ const Checkout = () => {
                         <input type="checkbox" className="hidden peer" />
                         <CustomCheckbox />
                         <label htmlFor="save-info">Save this information for next time</label>
+                    </div>
+                </section>
+                <section className='radios'>
+                    <div>
+                        <input type="radio" id="pay-card" className='custom-radio'/>
+                        <label htmlFor="pay-card"></label>
+                    </div>
+                    <div>
+                        <input type="radio" id="pay-delivery" className='custom-radio'/>
+                        <label htmlFor="pay-delivery"></label>
                     </div>
                 </section>
             </section>
