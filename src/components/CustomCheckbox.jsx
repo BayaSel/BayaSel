c4fe8aac62f3dc62c080ba7e5b313caa827f70a9
Siddami <dharmmy621@gmail.com>
2024-08-21 12:18:34 +0100

    removed custom label

--- src/components/CustomCheckbox.jsx
+++ src/components/CustomCheckbox.jsx
@@ -12,13 +12,12 @@ const CustomCheckbox = () => {
         onChange={() => setChecked(!checked)}
       />
       <div
-        className={`w-6 h-6 bg-gray-200 border-2 border-gray-300 rounded-md flex items-center justify-center ${
+        className={`w-6 h-6 bg-white border border-black rounded-md flex items-center justify-center ${
           checked ? "bg-white border-black" : ""
         }`}
       >
-        <i className={`bx bx-check ${checked ? "block" : "hidden"}`}></i>
+        <i className={`bx bx-check ${checked ? "block" : "hidden"} text-black`}></i>
       </div>
-      <span className="ml-2">My Custom Checkbox</span>
     </label>
   );
 };
