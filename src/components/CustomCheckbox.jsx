9e58240f5fcd6e87413acd729fedaab1fce7e43f
Siddami <dharmmy621@gmail.com>
2024-08-22 13:00:25 +0100

    make it more customized

--- src/components/CustomCheckbox.jsx
+++ src/components/CustomCheckbox.jsx
@@ -1,18 +1,26 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
-const CustomCheckbox = () => {
-  const [checked, setChecked] = useState(false);
+const CustomCheckbox = ({ checked: initialChecked = false, onChange, className = "" }) => {
+  const [checked, setChecked] = useState(initialChecked);
+
+  const handleChange = () => {
+    setChecked(!checked);
+    if (onChange) {
+      onChange(!checked);
+    }
+  };
 
   return (
-    <label className="inline-flex items-center cursor-pointer">
+    <label className={`inline-flex items-center cursor-pointer ${className}`}>
       <input
         type="checkbox"
         className="hidden"
         checked={checked}
-        onChange={() => setChecked(!checked)}
+        onChange={handleChange}
       />
       <div
-        className={`w-6 h-6 bg-white border border-black rounded-md flex items-center justify-center ${
+        className={`md:w-6 md:h-6 w-5 h-5 bg-white border border-black rounded-md flex items-center justify-center ${
           checked ? "bg-white border-black" : ""
         }`}
       >
