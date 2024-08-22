9b2a1db1ea949b23b938749e571b1f6e14c9751f
Siddami <dharmmy621@gmail.com>
2024-08-22 20:32:28 +0100

    added all components for checkout screen

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,12 +1,16 @@
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import CheckoutForm from "../components/Form/CheckoutForm"
+import Order from "../components/Order"
 
 const Checkout =()=>{
     return(
         <>
             <Header />
-            <CheckoutForm />
+            <div className="block md:flex md:mt-20">
+                <CheckoutForm />
+                <Order />
+            </div>
             <Footer/>
         </>
     )
