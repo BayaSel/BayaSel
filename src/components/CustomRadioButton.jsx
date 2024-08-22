3d2824c2b6cda3fe98dffadbefd2533e2c621e2e
Siddami <dharmmy621@gmail.com>
2024-08-22 13:29:13 +0100

    mobile responsiveness fixed

--- src/components/CustomRadioButton.jsx
+++ src/components/CustomRadioButton.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export default function CustomRadioButton({ isChecked, onChange, label }) {
   return (
-    <div className="flex items-center md:gap-8 gap-4 text-sm md:text-lg">
+    <div className="flex items-center font-semibold md:gap-6 gap-4 text-sm md:text-lg">
       <input
         type="radio"
         id={label}
@@ -12,7 +12,7 @@ export default function CustomRadioButton({ isChecked, onChange, label }) {
       />
       <label
         htmlFor={label}
-        className={`md:w-6 md:h-6 h-4 w-4 border ${
+        className={`md:w-6 md:h-6 h-4 w-4 border text-sm md:text-lg ${
           isChecked ? 'bg-white border-black' : 'border-black'
         } rounded-full flex items-center justify-center cursor-pointer`}
         onClick={onChange}
