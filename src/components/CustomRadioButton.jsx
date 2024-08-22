024550b1a80fec34549072bcaded4493acd1b1d1
Siddami <dharmmy621@gmail.com>
2024-08-22 12:05:44 +0100

    completed custom radio button

--- src/components/CustomRadioButton.jsx
+++ src/components/CustomRadioButton.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export default function CustomRadioButton({ isChecked, onChange, label }) {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-8">
       <input
         type="radio"
         id={label}
@@ -12,7 +12,7 @@ export default function CustomRadioButton({ isChecked, onChange, label }) {
       />
       <label
         htmlFor={label}
-        className={`w-6 h-6 border-2 ${
+        className={`w-6 h-6 border ${
           isChecked ? 'bg-white border-black' : 'border-black'
         } rounded-full flex items-center justify-center cursor-pointer`}
         onClick={onChange}
