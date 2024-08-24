41e60baa045c0e1c036f2be2ba65349825e662e2
Siddami <dharmmy621@gmail.com>
2024-08-24 10:52:16 +0100

    removed header and footer components from checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,17 +1,13 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
 import CheckoutForm from "../components/Form/CheckoutForm"
 import CheckoutOrder from "../components/CheckoutOrder"
 
 const Checkout =()=>{
     return(
         <>
-            <Header />
             <div className="block md:flex md:mt-20">
                 <CheckoutForm />
                 <CheckoutOrder />
             </div>
-            <Footer />
         </>
     )
 }
