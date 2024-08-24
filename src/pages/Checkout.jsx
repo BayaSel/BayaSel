102b4a88cb19723e04e50cab0491296d220d911e
Siddami <dharmmy621@gmail.com>
2024-08-24 10:46:17 +0100

    feat:fixed radio buttons alignment

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,12 +1,18 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 import CheckoutForm from "../components/Form/CheckoutForm"
 import CheckoutOrder from "../components/CheckoutOrder"
 
 const Checkout =()=>{
     return(
-        <div className="block md:flex md:mt-20">
-            <CheckoutForm />
-            <CheckoutOrder />
-        </div>
+        <>
+            <Header />
+            <div className="block md:flex md:mt-20">
+                <CheckoutForm />
+                <CheckoutOrder />
+            </div>
+            <Footer />
+        </>
     )
 }
 
